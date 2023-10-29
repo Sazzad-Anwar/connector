62f48aa1d0ec98072c7d4c78a922ba6351e67609
sazzad-anwar <sazzadzihan@gmail.com>
2023-10-30 01:35:54 +0600

    added icons for desktop app

--- src/components/nav/nav.tsx
+++ src/components/nav/nav.tsx
@@ -1,73 +1,80 @@
-import { useRef, useState } from "react";
+import { useRef, useState } from 'react'
 
-import { Button } from "../ui/button";
-import { Braces, Plus } from "lucide-react";
-import * as z from "zod";
-import { useForm, SubmitHandler } from "react-hook-form";
-import { zodResolver } from "@hookform/resolvers/zod";
-import { toast } from "../ui/use-toast";
-import useApiStore from "@/store/store";
-import { v4 as uuid } from "uuid";
-import { FolderType } from "../../types/api";
-import { Input } from "../ui/input";
-import useImportJSON from "@/hooks/useImportJSON";
-import { cn } from "@/lib/utils";
-import useSidepanelToggleStore from "@/store/sidePanelToggle";
-import AddCollectionDialog from "../collections/add-collection-dialog";
-import { SideNavHeader } from "./sidenav-header";
-import RenderNavigation from "./render-navigations";
+import useImportJSON from '@/hooks/useImportJSON'
+import { cn } from '@/lib/utils'
+import useSidepanelToggleStore from '@/store/sidePanelToggle'
+import useApiStore from '@/store/store'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { Braces, Plus } from 'lucide-react'
+import { SubmitHandler, useForm } from 'react-hook-form'
+import { v4 as uuid } from 'uuid'
+import * as z from 'zod'
+import { FolderType } from '../../types/api'
+import AddCollectionDialog from '../collections/add-collection-dialog'
+import { Button } from '../ui/button'
+import { Input } from '../ui/input'
+import { toast } from '../ui/use-toast'
+import RenderNavigation from './render-navigations'
+import { SideNavHeader } from './sidenav-header'
 
 export const CollectionSchema = z.object({
   collectionName: z
     .string()
-    .min(3, { message: "Collection name should be more than 3 characters" }),
-});
+    .min(3, { message: 'Collection name should be more than 3 characters' }),
+})
 
 type PropsType = {
-  isLoadingInSheet?: boolean;
-};
+  isLoadingInSheet?: boolean
+}
 
 export default function SideNav({ isLoadingInSheet }: PropsType) {
-  const buttonRef = useRef<HTMLButtonElement>(null);
-  const { InputFile } = useImportJSON();
-  const { isOpen } = useSidepanelToggleStore();
-  const [search, setSearch] = useState<string>("");
-  const { collections, createFolder, findOneFolder } = useApiStore();
+  const buttonRef = useRef<HTMLButtonElement>(null)
+  const { InputFile } = useImportJSON()
+  const { isOpen } = useSidepanelToggleStore()
+  const [search, setSearch] = useState<string>('')
+  const { collections, createFolder, findOneFolder } = useApiStore()
   const form = useForm<z.infer<typeof CollectionSchema>>({
-    mode: "onChange",
+    mode: 'onChange',
     resolver: zodResolver(CollectionSchema),
-  });
+  })
 
   const onSubmit: SubmitHandler<z.infer<typeof CollectionSchema>> = (data) => {
     const folder: FolderType = {
       name: data.collectionName,
-      type: "collection",
+      type: 'collection',
       isOpen: true,
       id: uuid(),
-    };
-    createFolder(folder);
-    buttonRef.current?.click();
-    form.reset();
+    }
+    createFolder(folder)
+    buttonRef.current?.click()
+    form.reset()
     toast({
-      variant: "success",
-      title: "Collection is saved",
-    });
-  };
+      variant: 'success',
+      title: 'Collection is saved',
+    })
+  }
 
   return (
     <aside
       className={cn(
-        "relative h-screen overflow-hidden border-r bg-background ",
-        isLoadingInSheet ? "w-full" : "w-0",
+        'relative h-screen overflow-hidden border-r bg-background ',
+        isLoadingInSheet ? 'w-full' : 'w-0',
         // isOpen ? "lg:w-[250px] xl:w-[300px]" : "hidden",
-        isOpen ? "w-full " : "hidden",
+        isOpen ? 'w-full ' : 'hidden',
       )}
     >
       <div className="h-full overflow-auto">
         <SideNavHeader />
         <div className="mb-3 flex items-center px-4 pb-0 pt-2">
-          <AddCollectionDialog type="collection" onSubmit={onSubmit}>
-            <Button variant="outline" size="xs" className="p-1">
+          <AddCollectionDialog
+            type="collection"
+            onSubmit={onSubmit}
+          >
+            <Button
+              variant="outline"
+              size="xs"
+              className="p-1"
+            >
               <Plus size={16} />
             </Button>
           </AddCollectionDialog>
@@ -76,11 +83,15 @@ export default function SideNav({ isLoadingInSheet }: PropsType) {
             value={search}
             placeholder="Search"
             onChange={(e) => {
-              setSearch(e.target.value);
-              findOneFolder(e.target.value);
+              setSearch(e.target.value)
+              findOneFolder(e.target.value)
             }}
           />
-          <InputFile variant="outline" size="xs" className="p-1">
+          <InputFile
+            variant="outline"
+            size="xs"
+            className="p-1"
+          >
             <Braces size={16} />
           </InputFile>
         </div>
@@ -90,9 +101,12 @@ export default function SideNav({ isLoadingInSheet }: PropsType) {
           </div>
         )}
         {collections?.map((collection: FolderType) => (
-          <RenderNavigation key={collection.id} collection={collection} />
+          <RenderNavigation
+            key={collection.id}
+            collection={collection}
+          />
         ))}
       </div>
     </aside>
-  );
+  )
 }
--- src/components/nav/sidenav-header.tsx
+++ src/components/nav/sidenav-header.tsx
@@ -1,14 +1,17 @@
-import { Link } from "react-router-dom";
-import { ThemeToggle } from "../theme-toggler";
+import { Link } from 'react-router-dom'
+import { ThemeToggle } from '../theme-toggler'
 
 export function SideNavHeader() {
   return (
     <div className="flex items-center justify-between gap-6 border-b p-5 md:gap-10">
-      <Link to="/" className="flex items-center space-x-2">
-        <i className="bi bi-plugin" />
-        <span className="inline-block font-bold">Connector</span>
+      <Link
+        to="/"
+        className="flex items-center space-x-2"
+      >
+        <i className="bi bi-plugin text-2xl" />
+        <span className="inline-block font-bold text-xl">Connector</span>
       </Link>
       <ThemeToggle />
     </div>
-  );
+  )
 }
