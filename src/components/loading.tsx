cd284c2ae4cf3e08cce2d9cd0e4b7e4a44c732fb
sazzad-anwar <sazzadzihan@gmail.com>
2023-11-01 15:36:34 +0600

    updated loading icon to connector icon

--- src/components/loading.tsx
+++ src/components/loading.tsx
@@ -1,17 +1,16 @@
-import { cn } from "@/lib/utils";
-import { Loader as LoaderSpinner } from "lucide-react";
+import { cn } from '@/lib/utils'
 
 export default function Loading({
   className,
   height,
 }: {
-  className?: string;
-  height?: number;
+  className?: string
+  height?: number
 }) {
   return (
     <div
       className={cn(
-        "flex min-w-[450px] items-center justify-center",
+        'flex min-w-[450px] items-center justify-center',
         className,
       )}
       style={{
@@ -19,11 +18,12 @@ export default function Loading({
       }}
     >
       <div className="flex flex-col items-center justify-center">
-        <LoaderSpinner size={25} className="animate-spin" />
+        {/* <LoaderSpinner size={25} className="animate-spin" /> */}
+        <i className="bi bi-plugin text-2xl animate-pulse" />
         <span className="flex animate-pulse items-center text-base">
           Connecting
         </span>
       </div>
     </div>
-  );
+  )
 }
